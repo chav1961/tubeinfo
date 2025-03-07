3a4bda188cb3314d1679f18065b019265d20c1ab
chav1961 <chav1961@mail.ru>
2025-03-07 10:56:17 +0500

    Daily commit

--- src/main/java/chav1961/tubeinfo/references/interfaces/TubesType.java
+++ src/main/java/chav1961/tubeinfo/references/interfaces/TubesType.java
@@ -28,7 +28,7 @@ public enum TubesType {
 	@LocaleResource(value="chav1961.tubeinfo.references.interfaces.tubesType.triodePentode",tooltip="chav1961.tubeinfo.references.interfaces.tubesType.triodePentode.tt",icon="triodePentode.png")
 	TRIODE_PENTODE(2, 1, "double_triode.svg"),
 	@LocaleResource(value="chav1961.tubeinfo.references.interfaces.tubesType.heptode",tooltip="chav1961.tubeinfo.references.interfaces.tubesType.heptode.tt",icon="heptode.png")
-	HEPTODE(1, 1, "double_triode.svg"),
+	HEPTODE(1, 1, "heptode.svg"),
 	@LocaleResource(value="chav1961.tubeinfo.references.interfaces.tubesType.triodeHeptode",tooltip="chav1961.tubeinfo.references.interfaces.tubesType.triodeHeptode.tt",icon="triodeHeptode.png")
 	TRIODE_HEPTODE(2, 1, "double_triode.svg"),
 	@LocaleResource(value="chav1961.tubeinfo.references.interfaces.tubesType.special",tooltip="chav1961.tubeinfo.references.interfaces.tubesType.special.tt",icon="special.png")
